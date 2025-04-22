899e327a4842e0cd699aa9e3790fd7dcd462a972
Christian Von Ryn <138238594+Verfuh@users.noreply.github.com>
2025-04-22 10:19:59 -0300

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 $data = json_decode(file_get_contents('php://input'), true);
 
 if (!isset($data['name']) || !isset($data['email']) || !isset($data['subject']) || !isset($data['message'])) {
-    echo json_encode(['success' => false, 'message' => 'Semua field harus diisi']);
+    echo json_encode(['success' => false, 'message' => 'Todos os campos devem ser preenchidos']);
     exit;
 }
 
